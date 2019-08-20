60ec72a2407bf81f27fe611a70ef557479a833d7
Mauro Mascarenhas <mauro.mascarenhas@nintersoft.com>
2019-08-20 14:29:41 -0300

    Added unigrams and bigrams dataframes

--- src/BullyingAnalyser.py
+++ src/BullyingAnalyser.py
@@ -11,6 +11,7 @@ print (csv_file)
 # Loads csv dataset file into a dataframe
 df = pd.read_csv(csv_file, ',')
 print (df.head(10))
+print ("\n")
 
 # Regex for username, links, and hashtags
 patterns = [
@@ -19,6 +20,9 @@ patterns = [
     (r"#", "")
 ]
 
+ungrmdf = pd.DataFrame(columns=['TEXTOS', 'y'])
+bigrmdf = pd.DataFrame(columns=['TEXTOS', 'y'])
+
 # Tokenize words from each tweet
 for index, rows in df.iterrows() :
     # Preprocessing of mentions, links and hashtags
@@ -26,7 +30,14 @@ for index, rows in df.iterrows() :
         rows['TEXTOS'] = re.sub(pat, sub, rows['TEXTOS'])
     rows['TEXTOS'] = nltk.word_tokenize(rows['TEXTOS'])
 
-    # Prints last tokenized items
-    if index > 1500 :
-        print(rows['TEXTOS'])
+    ungrmdf = ungrmdf.append(rows, ignore_index=True)
+    bigrmdf = bigrmdf.append({'TEXTOS' : list(nltk.bigrams(rows['TEXTOS'])), 'y' : rows['y']}, ignore_index=True)
 
+    # Prints last tokenized items/grams
+    if index > 1700 :
+        print("Uni:")
+        print(ungrmdf.loc[index])
+        print("Bi:")
+        print(bigrmdf.loc[index])
+
+print ("\n")
